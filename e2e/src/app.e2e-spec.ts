86ddfe58c4929e6822128d9f547146f8ab29c3c4
bruno <brunonyg@gmail.com>
2020-05-11 14:16:58 -0400

    Project name update

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('covidProject app is running!');
+    expect(page.getTitleText()).toEqual('myWebsite app is running!');
   });
 
   afterEach(async () => {
